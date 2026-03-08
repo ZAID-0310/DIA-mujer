61c402a35deb8ed0a27e48946599b6eb75da94a2
ZAID-0310 <izaidraul1507@gmail.com>
2026-03-07 19:52:41 -0500

    primera version

---
+++ script.js
@@ -0,0 +1,35 @@
+function abrirCarta(){
+
+let carta = document.getElementById("carta");
+
+carta.classList.toggle("abierta");
+
+/* mostrar fotos y video */
+
+let media = document.querySelectorAll(".media");
+
+media.forEach(item => {
+
+item.classList.toggle("visible");
+
+/* si es video lo reproducimos */
+
+if(item.tagName === "VIDEO"){
+item.play();
+}
+
+});
+
+}
+
+/* iniciar musica desde un tiempo */
+
+window.addEventListener("load", function(){
+
+let musica = document.getElementById("musica");
+
+/* tiempo en segundos */
+
+musica.currentTime = 29;
+
+});
